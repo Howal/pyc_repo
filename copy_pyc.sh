8579c367d842794d96f0add618f0aa9dfedbb340
yaozhuliang <yaozhuliang13@gmail.com>
2019-01-16 17:17:40 +0000

    resnet101 ae-aug symbol and yaml

--- copy_pyc.sh
+++ copy_pyc.sh
@@ -1,3 +1,3 @@
 python compile_all.py
-find ./ -not -name '*.py' -not -name '.git*' -not -path "./output/*" -not -path "./external/*" -not -path "./data/*" -not -path "*git/*" -not -path "*idea/*" | xargs cp --parents -t ../pyc_repo
+find ./ -not -name '*.py' -not -name '.git*' -not -path "./model/*" -not -path "./output/*" -not -path "./external/*" -not -path "./data/*" -not -path "*git/*" -not -path "*idea/*" | xargs cp --parents -t ../pyc_repo
 find ./ -path '*/symbols/*.py' | xargs cp --parents -t ../pyc_repo
