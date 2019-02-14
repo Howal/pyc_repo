9b01497fcde5265035455d5d2e361639a132d9b6
yaozhuliang <yaozhuliang13@gmail.com>
2019-02-14 02:41:18 +0000

    pyc

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ./common/lib/bbox
-python setup_linux.py build_ext --inplace
+python setup_linux.pyc build_ext --inplace
 cd ../dataset/pycocotools
-python setup_linux.py build_ext --inplace
+python setup_linux.pyc build_ext --inplace
 cd ../../nms
-python setup_linux.py build_ext --inplace
+python setup_linux.pyc build_ext --inplace
 cd ../..
 sudo /opt/conda/bin/pip install munkres
--- init_philly.sh
+++ init_philly.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ./common/lib/bbox
-/opt/conda/bin/python setup_linux.py build_ext --inplace
+/opt/conda/bin/python setup_linux.pyc build_ext --inplace
 cd ../dataset/pycocotools
-/opt/conda/bin/python setup_linux.py build_ext --inplace
+/opt/conda/bin/python setup_linux.pyc build_ext --inplace
 cd ../../nms
-/opt/conda/bin/python setup_linux.py build_ext --inplace
+/opt/conda/bin/python setup_linux.pyc build_ext --inplace
 cd ../..
 sudo /opt/conda/bin/pip install munkres
